c9954d270b7390475bc0ff39b8958f2f3f6d45e6
leomarkpaway <leomarkpaway@gmail.com>
2024-09-11 05:36:39 +0800

    :lipstick: Setup composes theme into main activity

--- app/src/main/java/com/leomarkpaway/compose_migration/MainActivity.kt
+++ app/src/main/java/com/leomarkpaway/compose_migration/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.leomarkpaway.compose_migration
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.leomarkpaway.compose_migration.ui.theme.ComposeTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            ComposeTheme {
+                // TODO add composable here
+            }
+        }
     }
+
 }
